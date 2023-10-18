59cf33d4db2b80061708ce590cc34f6e77fce8c6
nenad <nmijic@irb.hr>
2023-10-18 11:44:36 +0200

    gschol: fixing mpi warmup allreduce

--- src/gpu/gschol/gschol.cpp
+++ src/gpu/gschol/gschol.cpp
@@ -1,7 +1,7 @@
 #include <string>
 
-#include "cholesky_qr.hpp"
-#include "cqr2bgs.hpp"
+//#include "cholesky_qr.hpp"
+//#include "cqr2bgs.hpp"
 #include "gschol.hpp"
 
 cqr::gschol::gschol(std::int64_t m, std::int64_t n, std::int64_t panel_size) : 
@@ -75,8 +75,31 @@ cqr::gschol::~gschol()
 }
 
 
-void cqr::gschol::InputMatrix(std::vector<double> &A)
-{    
+void cqr::gschol::InputMatrix(cudamemory<double> &A)
+{
+    MPI_File fileHandle;
+    MPI_Status status;
+    int access_mode = MPI_MODE_RDONLY; // mode for reading only
+
+
+    if(MPI_File_open(mpi_comm_, filename_.data(), access_mode, MPI_INFO_NULL, &fileHandle) != MPI_SUCCESS)
+    {
+        std::cout << "Can't open input matrix - " << filename_ << " on rank " << world_rank_ << std::endl;
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    }
+
+    int displacement = distmatrix->get_rank_displacement();
+    int counts = distmatrix->get_rank_count();
+
+
+    MPI_File_read_at_all(fileHandle, displacement * n_* sizeof(double),
+                        A.data(), counts,
+                        distmatrix->get_datatype(), &status);
+
+    if (MPI_File_close(&fileHandle) != MPI_SUCCESS)
+    {
+        MPI_Abort(mpi_comm_, EXIT_FAILURE);
+    }
 }
 
 void cqr::gschol::InputMatrix(double *A)
@@ -132,29 +155,29 @@ void cqr::gschol::Start()
     std::vector<int> displacements = distmatrix->get_displacements();
     std::vector<int> counts = distmatrix->get_counts();
 
-    cudaAlocal_.copytohost(Alocal_);
-
+    /*
     MPI_Gatherv(Alocal_.data(),//cudaAlocal_.data(), 
                 localm_, distmatrix->get_datatype(), 
                 A_.data(),
                 counts.data(),
                 displacements.data(), 
                 distmatrix->get_datatype(), 0, mpi_comm_);
-
+    */  
+    validate = std::make_unique<Validate>(localm_, n_,
+                                            cudaAlocal_.data(), 
+                                            cudaR_.data(),
+                                            filename_.data(),
+                                            cublashandle_);
+    orthogonality_ = validate->orthogonality();
+    cudamemory<double> A(localm_ * n_);
+    InputMatrix(A);
+    residuals_     = validate->residuals(A);
     if( world_rank_ == 0)
-    {   
-        cudaR_.copytohost(R_);
-        validate = std::make_unique<Validate>(m_, n_,
-                                                 A_.data(), 
-                                                 R_.data(),
-                                                 filename_.data());
-        orthogonality_ = validate->orthogonality();
-        residuals_     = validate->residuals();
+    {
         std::cout << "orthogonality: " << orthogonality_ << std::endl;
         std::cout << "residuals: "     << residuals_     << std::endl;
         timing->print();
     }
-
 }
 
 
@@ -167,61 +190,41 @@ void cqr::gschol::gschol2(cudamemory<double> &A, cudamemory<double> &R)
     int number_of_panels = ceil((double)n_ / panel_size_);
 
     first_panel_orth();
-    std::string name = std::string("Q") + std::to_string(world_rank_) + std::string("_") + std::string(".bin");
-    //A.savematrix(name.data()); 
 
     for(int j = 1; j < number_of_panels; ++j )
     {   
         panel_size_ = (j == number_of_panels-1) ? n_ - (j) * panel_size_: panel_size_;
 
-        //if(j < number_of_panels - 1 )
         {
             NvtxTracer T("update");
             cudaWtmp_.memset();
-            //std::string name = std::string("Qub") + std::to_string(world_rank_) + std::string("_")  + std::string(".bin");
-            //A.savematrix(name.data());  
             update_rest_Matrix(A, j);
             save_R(cudaR_.data() + (j-1)*input_panel_size_*n_ + j*input_panel_size_,
                    n_, cudaWtmp_.data(), n_ - j * input_panel_size_, input_panel_size_, n_ - j * input_panel_size_);
-            //std::string name = std::string("R") + std::to_string(world_rank_) + std::string("_")  + std::string(".bin");
-            //cudaR_.savematrix(name.data());  
         }
 
         {
             cudatmp_.memset();
             NvtxTracer T("gram");
-            //std::string name = std::string("Qcb") + std::to_string(world_rank_) + std::string("_") + std::string(".bin");
-            //A.savematrix(name.data());
             gramMatrix(A.data() + j*input_panel_size_, 
                        cudaR_.data() + j*input_panel_size_*n_ + j*input_panel_size_,
                        cudatmp_.data());
             cholesky(cudatmp_.data());
             calculateQ(A.data() + j*input_panel_size_, 
                        cudatmp_.data());
-            //save_triangular_R(cudaR_.data() + j*input_panel_size_*n_ + j*input_panel_size_,
-            //       n_, cudatmp_.data(), panel_size_, panel_size_, panel_size_);
             save_R(cudaR_.data() + j*input_panel_size_*n_ + j*input_panel_size_,
                    n_, cudatmp_.data(), panel_size_, panel_size_, panel_size_);
-            //save_R(cudaR_.data() + j*input_panel_size_*n_ + j*input_panel_size_, cudatmp_.data());
-            //name = std::string("Qca") + std::to_string(world_rank_) + std::string("_") + std::string(".bin");
-            //A.savematrix(name.data());
         }
 
-        //if(j < number_of_panels - 1 )
         {
             NvtxTracer T("update");
-            //std::string name = std::string("Qrb") + std::to_string(world_rank_) + std::string("_") + std::to_string(j) + std::string(".bin");
-            //A.savematrix(name.data());
             reothrogonalize_panel(A, j);
-            //name = std::string("Qra") + std::to_string(world_rank_) + std::string("_") + std::to_string(j) + std::string(".bin");
-            //A.savematrix(name.data());
+
         }
 
         {
             cudatmp_.memset();
             NvtxTracer T("gram");
-            //std::string name = std::string("Qcb") + std::to_string(world_rank_) + std::string("_") + std::string(".bin");
-            //A.savematrix(name.data());
             gramMatrix(A.data() + j*input_panel_size_, 
                        cudaR2_.data() + j*input_panel_size_*n_ + j*input_panel_size_,
                        cudatmp_.data());
@@ -229,26 +232,8 @@ void cqr::gschol::gschol2(cudamemory<double> &A, cudamemory<double> &R)
             calculateQ(A.data() + j*input_panel_size_, 
                        cudatmp_.data());
             multiply_R(cudaR_.data() + j*input_panel_size_*n_ + j*input_panel_size_, cudatmp_.data());
- 
         }
     }
-    //A.savematrix("Q0.bin");
-    /*
-    {
-        timing->start_timing("computation");
-        NvtxTracer T("R2R1");
-        CUBLAS_CHECK(cublasDgemm(cublashandle_,
-                                CUBLAS_OP_N, CUBLAS_OP_N,
-                                n_,  n_,  n_,
-                                &alpha,
-                                cudaR1_.data(), n_,
-                                cudaR2_.data(), n_,
-                                &beta, cudaR_.data(), n_));
-        timing->stop_timing("computation");
-    }
-    */
-   //cudaR_.savematrix("R.bin");
-   //A.savematrix("Q.bin");
 }
 
 
@@ -260,15 +245,10 @@ void cqr::gschol::first_panel_orth()
         gramMatrix(cudaAlocal_.data(), 
                    cudaR_.data(),
                    cudatmp_.data());
-        // saving tmp to R
-        //cudatmp_.savematrix("tmp.bin");
         cholesky(cudatmp_.data());
-        //cudatmp_.savematrix("chol.bin");
         calculateQ(cudaAlocal_.data(), 
                    cudatmp_.data());
         save_R(cudaR_.data(), n_, cudatmp_.data(), panel_size_, panel_size_, panel_size_);
-        //save_triangular_R(cudaR_.data(), n_, cudatmp_.data(), panel_size_, panel_size_, panel_size_);
-
     }
     {
         cudatmp_.memset();
@@ -369,77 +349,11 @@ void cqr::gschol::calculateQ(double *A, double *R)
 }
 
 
-void cqr::gschol::updateMatrix(int n, int ldw, double *A, double *R)
-{
-    double alpha = 1.0, beta=0.0;
-
-    int m = localm_;
-    int lda = n_, ldr = n_;
-
-    // W = Qj^T @ Ar
-    timing->start_timing("computation");
-    CUBLAS_CHECK(cublasDgemm(cublashandle_,
-                             CUBLAS_OP_N, CUBLAS_OP_T,
-                             n, panel_size_, m,
-                             &alpha,
-                             A+panel_size_, lda,
-                             A, lda,
-                             &beta,
-                             cudaWtmp_.data(), ldw));
-    timing->stop_timing("computation");
-    
-    timing->start_timing("communication");
-    #ifdef NCCL
-        NCCLCHECK(ncclAllReduce(cudaWtmp_.data(), cudaWtmp_.data(), ldw*panel_size_, ncclDouble, ncclSum,  nccl_comm_, 0));
-    #else
-        MPI_Allreduce(MPI_IN_PLACE, cudaWtmp_.data(), ldw*panel_size_, MPI_DOUBLE, MPI_SUM, mpi_comm_);
-    #endif
-    timing->stop_timing("communication");
-
-    alpha = -1.0;
-    beta = 1.0;
-
-    timing->start_timing("computation");
-    CUBLAS_CHECK(cublasDgemm(cublashandle_,
-                             CUBLAS_OP_N, CUBLAS_OP_N,
-                             n, m, panel_size_,
-                             &alpha,
-                             cudaWtmp_.data(), ldw,
-                             A, lda,
-                             &beta,
-                             A+panel_size_, lda));
-
-    alpha = 1.0;
-    CUBLAS_CHECK(cublasDtrmm(cublashandle_,
-                             CUBLAS_SIDE_LEFT, CUBLAS_FILL_MODE_UPPER,
-                             CUBLAS_OP_N, CUBLAS_DIAG_UNIT,
-                             n, panel_size_,
-                             &alpha,
-                             cudaI_.data(), lda,
-                             cudaWtmp_.data(), ldw,
-                             R, ldr));
-    timing->stop_timing("computation");
-}
-
 void cqr::gschol::save_R(double* R, std::size_t ldr, double* tmp, std::size_t ldtmp, int m, int n)
 {
     timing->start_timing("computation");
     cudaMemcpy2D(R, sizeof(double) * ldr, tmp, sizeof(double) * ldtmp, sizeof(double) * n, m, cudaMemcpyDeviceToDevice);
     timing->stop_timing("computation");
-}
-
-void cqr::gschol::save_triangular_R(double *R, std::size_t ldr, double *tmp, std::size_t ldtmp, int m, int n)
-{
-    double alpha = 1.0;
-    CUBLAS_CHECK(cublasDtrmm(cublashandle_,
-                             CUBLAS_SIDE_LEFT, CUBLAS_FILL_MODE_UPPER,
-                             CUBLAS_OP_N, CUBLAS_DIAG_UNIT,
-                             n, m,
-                             &alpha,
-                             cudaI_.data(), n,
-                             tmp, ldtmp,
-                             R, ldr));
-
 }
 
 void cqr::gschol::multiply_R(double *R, double *tmp)
@@ -548,10 +462,8 @@ void cqr::gschol::update_rest_Matrix(cudamemory<double> &A, int panel_number)
 void cqr::gschol::MPI_Warmup()
 {
 #ifdef GPU
-    if (localm_ >= n_)
-        MPI_Allreduce(MPI_IN_PLACE, cudaWtmp_.data(), n_ * n_, MPI_DOUBLE, MPI_SUM, mpi_comm_);
-    else
-        MPI_Allreduce(MPI_IN_PLACE, cudaWtmp_.data(), 10 * n_, MPI_DOUBLE, MPI_SUM, mpi_comm_);
+    MPI_Allreduce(MPI_IN_PLACE, cudaWtmp_.data(), (n_ -input_panel_size_), MPI_DOUBLE, MPI_SUM, mpi_comm_);
+
 #else
     //MPI_Allreduce(MPI_IN_PLACE, cudaWtmp1_.data(), n_ * n_, MPI_DOUBLE, MPI_SUM, mpi_comm_);
 #endif
